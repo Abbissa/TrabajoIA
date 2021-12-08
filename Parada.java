5ae8df0e14a93c3208457336a42b1a06155f539e
CarlosEvangelista <43065083+CarlosEvangelista@users.noreply.github.com>
2021-12-08 20:49:08 +0100

    Añadido constructor de copia, atributos g,h, y parent, además del metodo equals y f

--- Parada.java
+++ Parada.java
@@ -13,13 +13,52 @@ public  class Parada{
 	int id;	
 	ArrayList<Conexion> conexiones;
 	Color color;
+	Parada parent;
+	double g;
+	double h;
+
 	public Parada(String nombre, Float x, Float y, Color color) {
 
-		this.conexiones=new ArrayList<Conexion>(2);
+		this.conexiones=new ArrayList<>(2);
 		this.color=color;
 		btn = new JCheckBox();
 		this.nombre=nombre;
 		this.x=x;
 		this.y=y;
+		this.parent = null;
+		this.g = 0;
+		this.h = 0;
 	}
+
+	public Parada(Parada p) {
+		this.conexiones=p.conexiones;
+		this.color=p.color;
+		btn = p.btn;
+		this.nombre=p.nombre;
+		this.x=p.x;
+		this.y=p.y;
+		this.parent = p.parent;
+		this.g = p.g;
+		this.h = p.h;
+	}
+
+	//Calcular g(x) + h(x) 
+	public double f(){
+		return this.g + this.h;
+	}
+
+
+	@Override
+	public boolean equals(Object otro){
+		if(otro==null){
+			return false;
+		}
+		if(!(otro instanceof Parada)){
+			return false;
+		}
+		Parada p = (Parada)otro;
+		return this.nombre.equals(p.nombre);
+	}
+
+	
 }
