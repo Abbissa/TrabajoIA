3ee0a75c5c02c41947970fd056397b3668331020
carilinaaltamirano <55328515+carilinaaltamirano@users.noreply.github.com>
2021-12-11 12:05:15 +0100

    Update Conexion.java

--- Conexion.java
+++ Conexion.java
@@ -1,8 +1,8 @@
 public class Conexion {
 	
 	Parada destino;		// destino de la conexion
-	double distancia;	// distancia a recorrer, no aerea, en metros o kilometros, por determinar 
-	double velocidad;	// velocidad de tren o de una persona al andar, dependiendo de que conexion sea, pueden plantearse 
+	double distancia;	// distancia a recorrer Km
+	double velocidad;	// velocidad de tren o de una persona al andar, dependiendo de que conexion sea, pueden plantearse  Km/h
 						// como variables de CalcularPuntosMapa, depende de como lo hagamos
 						
 	double tdeEspera;	// tiempo de espera medio hasta que llega un tren a esa conexion, 0 en el caso de trasbordo, no se si es necesario implementarlo
